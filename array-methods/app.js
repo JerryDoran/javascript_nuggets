9e1ba28e0abd71aac7d7d467fd9c7be0a489b123
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-10-29 12:36:01 -0400

    more on reduce method

--- array-methods/app.js
+++ array-methods/app.js
@@ -36,3 +36,76 @@ const dailyTotal = people.reduce((total, person) => {
 }, 0);
 
 console.log(dailyTotal);
+
+// cart example
+const cart = [
+  {
+    title: 'Samsung galaxy S7',
+    price: 599.99,
+    amount: 1,
+  },
+  {
+    title: 'Google Pixel',
+    price: 499.99,
+    amount: 2,
+  },
+  {
+    title: 'Iphone 11',
+    price: 699.99,
+    amount: 4,
+  },
+  {
+    title: 'Xiami Redmi Note 2',
+    price: 599.99,
+    amount: 3,
+  },
+];
+
+// this will return an object as the second parameter to the reduce method.
+let { totalItems, cartTotal } = cart.reduce(
+  (total, cartItem) => {
+    const { amount, price } = cartItem;
+    // count items
+    total.totalItems += amount;
+    // get value of cart (sum amount of each item * price)
+    total.cartTotal += amount * price;
+
+    return total;
+  },
+  {
+    totalItems: 0,
+    cartTotal: 0,
+  }
+);
+
+cartTotal = parseFloat(cartTotal.toFixed(2));
+// console.log(total);
+console.log(totalItems, cartTotal);
+
+// github repos example
+const url = 'https://api.github.com/users/john-smilga/repos?per_page=100';
+
+const fetchRepos = async () => {
+  const res = await fetch(url);
+  const repos = await res.json();
+  const newData = repos.reduce((total, repo) => {
+    const { language } = repo;
+
+    // set up dynamic property
+    // if (language) {
+    //   if (total[language]) {
+    //     total[language] = total[language] + 1;
+    //   } else {
+    //     total[language] = 1;
+    //   }
+    // }
+
+    if (language) {
+      total[language] = total[language] + 1 || 1;
+    }
+    return total;
+  }, {});
+  console.log(newData);
+};
+
+fetchRepos();
